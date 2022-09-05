71363bd925b1ab0ba9cfbed09ee021ee0709f8f4
Ludovico Colucci <ludovicocolucci@gmail.com>
2022-09-05 16:41:32 +0200

    add ex from 8 to 13

--- 09/app.test.js
+++ 09/app.test.js
@@ -1,6 +1,6 @@
 const supertest = require("supertest");
 
-const createApp = require("./app.js.js");
+const createApp = require("./app.js");
 
 const app = createApp();
 
@@ -10,7 +10,8 @@ test("GET /", async () => {
   const response = await request
     .get("/")
     .expect(200)
-    .expect("Content-Type", "application/json");
+    .expect("Content-Type", "text/html");
 
-  expect(response.body).toEqual({ location: "Earth" });
+    expect(response.text).toEqual("Welcome to the World Wide Web!");
+  
 });
